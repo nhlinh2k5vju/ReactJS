25f74ac16d51dda5e3de811dc0c9a9e968ad366d
Denny Nguyen <linh.nguyenhai0909@gmail.com>
2025-09-29 15:10:46 +0700

    Vid31

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,9 +1,12 @@
 import './style.css'
 
+const linh = [1, 2, 3]
+
 const MyFunction = () => {
     return (
       <>
-        <div>Dep trai </div>
+        <div>{JSON.stringify(linh)} \t {linh.age} Dep trai </div>
+        <div>{console.log("Emper")}</div>
         <div className='child'>co gi sai?</div>
       </>
     );
